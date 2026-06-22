71a980734d38f402207f7e1c24181892de952415
Breno Rezende Pinto <brenozende@gmail.com>
2026-06-22 20:08:55 -0300

    Client endpoints implemented

--- src/main/java/br/com/galleriabank/desafio/service/ClientService.java
+++ src/main/java/br/com/galleriabank/desafio/service/ClientService.java
@@ -1,9 +1,12 @@
 package br.com.galleriabank.desafio.service;
 
 import br.com.galleriabank.desafio.exception.BusinessException;
+import br.com.galleriabank.desafio.exception.ResourceNotFoundException;
 import br.com.galleriabank.desafio.model.dto.request.CreateClientRequest;
 import br.com.galleriabank.desafio.model.entity.Client;
 import br.com.galleriabank.desafio.repository.ClientRepository;
+import br.com.galleriabank.desafio.repository.OrderRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ClientService {
     private final ClientRepository clientRepository;
+    private final OrderRepository orderRepository;
 
     public Client createClient(CreateClientRequest request) {
         if (clientRepository.existsByCpf(request.cpf()))
@@ -22,5 +26,28 @@ public class ClientService {
         client.setPhoneNumber(request.phoneNumber());
 
         return clientRepository.save(client);
+    }
+
+    public Client findClient(Long id) {
+        return clientRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Client not found"));
+    }
+
+    public Client updateClient(Long id, @Valid CreateClientRequest request) {
+        Client client = findClient(id);
+        client.setPhoneNumber(request.phoneNumber());
+        client.setCpf(request.cpf());
+        client.setName(request.name());
+        return clientRepository.save(client);
+    }
+
+    public void deleteClient(Long id) {
+        Client client = findClient(id);
+
+        if (orderRepository.existsByClientId(id)) {
+            throw new BusinessException("Client has orders and cannot be deleted.");
+        }
+
+        clientRepository.delete(client);
     }
 }
